ab8ce77589d4348503a6156dfa8df6e320799521
Aishwarya1544 <160617665+Aishwarya1544@users.noreply.github.com>
2024-02-27 13:34:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+let number1 = 5;
+let number2 = 7;
+console.log(number1 + number2);
+let number3 = 7;
+let number4 = 3;
+console.log(number2 - number4);
+let number5 = 2;
+let number6 = 6;
+console.log(number5 * number6);
+let number7 = 7;
+let number8 = 2;
+console.log(number7 / number8);
+
+// assignment operators
+console.log(!(number7 == 5 && number8 ==6));
+
+//javascript functions
+function run() {
+console.log("running");
+}
+run();
+function jog() {
+    console.log("jogging");
+}
+jog();// calling the function
+
+//if else statments
+
+let count = 638;
+if (count < 200) {
+    console.log("Not okay to go");
+}
+else if (count < 300){
+     console.log("can start");
+}
+else {
+    console.log("okay to go");
+}
+//for loops
+for(i = 10; 1>= 0; i++){
+    console.log("I am a hero");
+}  
+//while loop
+let number =0;
+ while(number < 10){
+    console.log("Hello world");
+    number++;
+ }
+ //strings
+ let fname = "Aishwarya";
+ let lname = "janamanchi";
+ console.log(fname + " " + lname );
+ // lowercase
+ let fname = "AISHWARYA";
+ console.log(fname.toLowerCase());
+ //uppercase
+ let fname = "janamanchi";
+ console.log(fname.toUpperCase());
+
+
+
+
+
+
+
+
+
